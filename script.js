7d610058a3396c735bf0e9f4063738e9d5638440
iAmMuadh <adeyemimuaz1@gmail.com>
2023-02-13 13:51:30 +0100

    working on a chrome extension

---
+++ script.js
@@ -0,0 +1,134 @@
+let myLeads = ['www.myLeads.com'];
+
+let inputEl = document.getElementById('input-el');
+
+
+//A NEATER WAY TO ADD EVENT LISTENERS TO YOUR CODE
+let inputBtn = document.getElementById("input-btn");
+const ulEl = document.querySelector('#ul-el');
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+console.log(leadsFromLocalStorage)
+
+inputBtn.addEventListener('click' , function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    renderLeads();
+});
+
+
+function renderLeads() {
+    let listItems= '';
+    for (let i = 0;  i < myLeads.length; i++) {
+
+        listItems = `
+        <li>
+            <a  target = '_blank' href = ${myLeads[i]}>
+            ${myLeads[i]};
+        </li>
+        `
+    };
+    ulEl.innerHTML += listItems;
+};
+
+
+
+localStorage.setItem('myLeadz', 'www.exampleLead.com');
+
+console.log( localStorage.getItem('myLeadz')) ;
+
+localStorage.clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TEMPLATE STRING/LITERALS PRACTICE
+const recipient = 'James';
+const sender = 'Muadhuuuuu'
+
+const email = `
+               Hey ${recipient}!
+               How's it going?? 
+               Cheers ${sender}!!!
+            `;
+console.log(email)
+
+
+
+// function clearInput() {
+//     let clearBtn = document.getElementById('clear-btn');
+//     if (clearBtn.value == listItems) {
+//         clearBtn.value = '';
+//     }
+// }
+
+
+//.innerHTML
+
+// const containerEl = document.getElementById("container");
+// containerEl.innerHTML = '<button>BUY</button>';
+
+
+// containerEl.addEventListener('click' , function clickMe() {
+//     containerEl.innerHTML += '<p>Thank you for purchasing with us</p>'
+// });
+
+
+// PRACTICE 1 
+// let theBoxOpener = document.getElementById("boxEl");
+
+// theBoxOpener.addEventListener('click' , function giftPackage() {
+//     console.log("I want to open the box!!")
+// });
+
+// PRACTICE 2 (USING CONST)
+// const basePrice = 520;
+// const discount = 120;
+// let shippingCost = 12;
+// let shippingTime = '5-12 days';
+
+
+// shippingCost = 15;
+// shippingTime = '7-14 days';
+
+// const fullPrice = basePrice - discount + shippingCost;
+
+// console.log('Total Cost: ' + fullPrice + '. It will arrive in ' + shippingTime);
+
+
+
+
+
